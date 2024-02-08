97960919714ada464aa78d47159b3d5993aa4a44
Kiet Nguyen <giakietnt2205@gmail.com>
2024-02-08 22:57:13 +0700

    order: base

--- src/controllers/Order.controller.js
+++ src/controllers/Order.controller.js
@@ -1,2 +1,30 @@
 const OrderService = require('../services/order.service')
 const {StatusCodes} = require('http-status-codes');
+
+const getAllOrders = async(req, res) => {
+    res.json(`ala`)
+}
+
+const getSingleOrder = async(req, res) => {
+    res.json(`ala`)
+}
+
+const getCurrentUserOrders = async(req, res) => {
+    res.json(`ala`)
+}
+
+const CreateOrder = async(req, res) => {
+    res.json(`ala`)
+}
+
+const UpdateOrder = async(req, res) => {
+    res.json(`ala`)
+}
+
+module.exports = {
+    getAllOrders,
+    getSingleOrder,
+    getCurrentUserOrders,
+    CreateOrder,
+    UpdateOrder
+}
--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,5 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require('../middleware/authentication');
 
+const {
+    getAllOrders,
+    getSingleOrder,
+    getCurrentUserOrders,
+    CreateOrder,
+    UpdateOrder
+} = require('../controllers/Order.controller')
+
+
+router
+  .route('/')
+  .post(authenticateUser, CreateOrder)
+  .get(authenticateUser, authorizePermissions('admin'), getAllOrders);
+
+router.route('/showAllMyOrders').get(authenticateUser, getCurrentUserOrders);
+
+router
+  .route('/:id')
+  .get(authenticateUser, getSingleOrder)
+  .patch(authenticateUser, UpdateOrder);
 
 module.exports = router;
